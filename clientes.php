90833cef1629bf4b885b44b27d2cdb03452f9d20
denissesalas26 <vanessasalassaavedra26@gmail.com>
2023-11-28 12:59:49 -0500

    ssd

--- clientes.php
+++ clientes.php
@@ -8,12 +8,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes</title>
+    <title>Carrito de Compras</title>
     <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+
+<!-- Plugins css -->
+    <link href="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
     
+
     
+    <link rel="shortcut icon" href="carrito.png">
 </head>
 
 <body>
@@ -30,39 +39,42 @@
             <div class="row">
                 <div class="col-12">
                     <div class="col-10">
-                    <?php  
-                      include("modal/modal_frm_reg_clientes.php");
+                      <?php  
+                      include("include/modal_frm_reg_clientes.php");
                       ?>
-                   <!--- <h4>Registrar Clientes</h4>-->
+
+                   
+                   <!---- <h4>Registrar Usuarios</h4>--->
                     </div>
-                    <!----
+                    <!---
                     <div class="card">
                         <div class="card-body">
 
-                        <form action="registrar_clientes.php" method="post">
-
-
-                        <div class="form-group row ">
-                                <label class="col-lg-2 col-md-2 col-sm-12" for="">ruc_dni :</label>
-                                <input type="number"class="form-control col-lg-4 col-md-4 col-sm-12"  name="ruc"placeholder="Ingrese el Telefono "required>
+                        <form action="registrar_usuarios.php" method="post" enctype="multipart/form-data">
+                            <div class="form-group row ">
+                                <label class="col-lg-2 col-md-2 col-sm-12" for="">DNI:</label>
+                                <input type="number"class="form-control col-lg-4 col-md-4 col-sm-12"  name="dni" autocomplete="off"placeholder="Ingrese su Dni " required >
 
                             </div>
-                        <div class="form-group row">
-                            <label class="col-lg-2 col-md-2 col-sm-12" for="">Razon Social</label>
-                            <input type="text"class="form-control ol-lg-10 col-md-10 col-sm-12"  name="nombre"placeholder="Ingrese  nombre  "required>
+
+
+
+                            <div class="form-group row">
+                            <label class="col-lg-2 col-md-2 col-sm-12" for="">Apellidos y Nombres</label>
+                            <input type="text"class="form-control ol-lg-10 col-md-10 col-sm-12"  name="a_n"placeholder="Ingrese su apellido y su nombre  "required>
 
                             </div>
 
                             <div class="form-group row ">
                                 <label class="col-lg-2 col-md-2 col-sm-12" for="">Correo:</label>
-                                <input type="email"class="form-control col-lg-10 col-md-10 col-sm-12"  name="correo"placeholder="Ingrese el Correo"required>
+                                <input type="email"class="form-control col-lg-10 col-md-10 col-sm-12"  name="correo"placeholder="Ingrese su Correo"required>
 
                             </div>
 
 
                             <div class="form-group row ">
                                 <label class="col-lg-2 col-md-2 col-sm-12" for="">Telefono :</label>
-                                <input type="number"class="form-control col-lg-4 col-md-4 col-sm-12"  name="telefono"placeholder="Ingrese el Telefono "required>
+                                <input type="number"class="form-control col-lg-4 col-md-4 col-sm-12"  name="telefono"placeholder="Ingrese su Telefono "required>
 
                             </div>
 
@@ -71,20 +83,36 @@
 
                             <div class="form-group row ">
                                 <label class="col-lg-2 col-md-2 col-sm-12" for="">Direccion:</label>
-                                <input type="text"class="form-control ol-lg-10 col-md-10 col-sm-12"  name="direccion"placeholder="Ingrese la Direccion"required>
+                                <input type="text"class="form-control ol-lg-10 col-md-10 col-sm-12"  name="direccion"placeholder="Ingrese su Direccion"required>
 
                             </div>
+
 
 
 
                             <div class="form-group row ">
-                                <label class="col-lg-2 col-md-2 col-sm-12" for="">Direccion de Envio</label>
-                                <input type="text"class="form-control col-lg-4 col-md-4 col-sm-12"  name="envio"placeholder="Ingrese el Direccion de envio "required>
+                                <label class="col-lg-2 col-md-2 col-sm-12" for="">Fecha de Nacimiento:</label>
+                                <input type="date"class="form-control col-lg-4 col-md-4 col-sm-12"  name="nacimiento"placeholder="Ingrese su Fecha de Nacimiento "required>
+
+                            </div>
+                            <div class="form-group row ">
+                                <label class="col-lg-2 col-md-2 col-sm-12" for="">Foto</label>
+                                <input type="file"class="form-control col-lg-4 col-md-4 col-sm-12" accept="image/*"  name="img"placeholder="Ingrese su Fecha de Nacimiento "required>
 
                             </div>
 
+                            <div class="form-group row ">
+                                <label class="col-lg-2 col-md-2 col-sm-12" for="">Rol</label>
+                                
+                             <select name="rol" class="form-control col-lg-4 col-md-4 col-sm-12" id="" type="file">
+                           
+                            
+                             </select>
+                            </div>
 
-                    
+
+
+                            
 
                             <div class="form-group row ">
                                 <label class="col-lg-2 col-md-2 col-sm-12" for=""></label>
@@ -97,11 +125,13 @@
 
 
                         </form>
+                  
                         </div>
                         
                     </div>
+                    ---->
 
----->
+
 
 
 
@@ -115,21 +145,19 @@
                         
 
 
+                        
+                       
+    
+<?php
+
+require('conexion.php');
+
+
+?>
 
 
 
 
-
-
-
-
-
-
-
-
-<br>
-<br>
-<br>
 <table id="basic-datatable" class="table dt-responsive nowrap">
   <thead>
     <tr>
@@ -137,11 +165,12 @@
       <th scope="col">id</th>
       <th scope="col">Ruc_Dni</th>
       <th scope="col">Razon Social</th>
-      <th scope="col">Telefono/th>
+      <th scope="col">Telefono</th>
       <th scope="col">Correo</th>
+      
       <th scope="col">Direccion</th>
       <th scope="col">Direccion de envio</th>
-  
+
       <th scope="col">Accion</th>
     </tr>
   
@@ -150,6 +179,7 @@
   <tbody>
   <?php
 $consulta="select *from cliente ";
+
 $ejecutar=mysqli_query($conn,$consulta);
 $cont=0;
 
@@ -160,7 +190,7 @@ echo"<tr>";
 echo"<td>".$cont."</td>";
 echo"<td>".$respuesta['id']."</td>";
 echo"<td>".$respuesta['ruc_dni']."</td>";
-echo"<td>".$respuesta['razon_social']."</td>";
+echo"<td>".$respuesta['razon Social']."</td>";
 echo"<td>".$respuesta['telefono']."</td>";
 echo"<td>".$respuesta['correo']."</td>";
 echo"<td>".$respuesta['direccion']."</td>";
@@ -185,6 +215,9 @@ echo"</tr>";
 ?>
   </tbody>
 </table>
+
+
+
 <!-- table -->
                         </div>
                         
@@ -229,6 +262,46 @@ echo"</tr>";
     <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
 
+    <!-- App js -->
+    <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- jQuery  -->
+    <script src="plantilla/Admin/assets/js/jquery.min.js"></script>
+    <script src="plantilla/Admin/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="plantilla/Admin/assets/js/metismenu.min.js"></script>
+    <script src="plantilla/Admin/assets/js/waves.js"></script>
+    <script src="plantilla/Admin/assets/js/simplebar.min.js"></script>
+
+    <!-- third party js -->
+    <script src="plantilla/Admin/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.responsive.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/responsive.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.buttons.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.html5.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.flash.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.print.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.keyTable.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.select.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/pdfmake.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/vfs_fonts.js"></script>
+    <!-- third party js ends -->
+
+    <!-- Datatables init -->
+    <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
+    
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
 
