0357e275015625d25caed1ad14399b86e743930f
denissesalas26 <vanessasalassaavedra26@gmail.com>
2023-11-29 09:51:25 -0500

    asdvbv
    sdfbnn

--- include/header.php
+++ include/header.php
@@ -32,7 +32,7 @@
                         <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
                     </button>
 
-                    <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-menu dropdown-menu-left">
 
                         <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
                              Perfil
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    require('conexion.php');
-    
-    ?>
+require('conexion.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,39 +14,39 @@
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 
 
-<!-- Plugins css -->
+    <!-- Plugins css -->
     <link href="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
-    
 
-    
-   
+
+
+
 </head>
 
 <body>
     <?php
     require('include/header.php');
     ?>
-     
-     <!---INICIO DE CONTENIDO--->
-<div class="main-content">
+
+    <!---INICIO DE CONTENIDO--->
+    <div class="main-content">
 
 
-   <div class="page-content">
-       <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="col-10">
-                      <?php  
-                      include("modal/modal_frm_reg_usuario.php");
-                      ?>
+        <div class="page-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="col-10">
+                            <?php
+                            include("modal/modal_frm_reg_usuario.php");
+                            ?>
 
-                   
-                   <!---- <h4>Registrar Usuarios</h4>--->
-                    </div>
-                    <!---
+
+                            <!---- <h4>Registrar Usuarios</h4>--->
+                        </div>
+                        <!---
                     <div class="card">
                         <div class="card-body">
 
@@ -107,18 +107,18 @@
                              <select name="rol" class="form-control col-lg-4 col-md-4 col-sm-12" id="" type="file">
                            <optio value=""></optio>
                              <?php
-                             $consulta_rol="SELECT*FROM roles";
-                             $ejecutar= mysqli_query($conn, $consulta_rol);
-                             while($datos_roles=mysqli_fetch_array($ejecutar)){?>
+                                $consulta_rol = "SELECT*FROM roles";
+                                $ejecutar = mysqli_query($conn, $consulta_rol);
+                                while ($datos_roles = mysqli_fetch_array($ejecutar)) { ?>
                             
 
 
-                              <option value="<?php echo  $datos_roles['id'];?>"><?php  echo $datos_roles['nombre'];?></option>
+                              <option value="<?php echo  $datos_roles['id']; ?>"><?php echo $datos_roles['nombre']; ?></option>
                           
                              
                              
                             
-                             <?php }?>
+                             <?php } ?>
                             
                              </select>
                             </div>
@@ -152,120 +152,116 @@
 
 
 
-                    <div class="card">
-                        <div class="card-body">
-
-                        
-
-
-                        
-                       
-    
-<?php
-
-require('conexion.php');
-
-
-?>
-
-
-
-
-<table id="basic-datatable" class="table dt-responsive nowrap">
-  <thead>
-    <tr>
-     <th scope="col">#</th>
-      <th scope="col">id</th>
-      <th scope="col">Dni</th>
-      <th scope="col">Apellidos_Nombres</th>
-      <th scope="col">Correo</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Direccion</th>
-      <th scope="col">Rol</th>
-      <th scope="col">Imagen</th>
-      <th scope="col">Accion</th>
-    </tr>
-  
-
-  </thead>
-  <tbody>
-  <?php
-$consulta="select *from usuario ";
-$ejecutar=mysqli_query($conn,$consulta);
-$cont=0;
-
-while($respuesta=mysqli_fetch_array($ejecutar)){
-    $cont++;
-   
-echo"<tr>";
-echo"<td>".$cont."</td>";
-echo"<td>".$respuesta['id']."</td>";
-echo"<td>".$respuesta['dni']."</td>";
-echo"<td>".$respuesta['apellidos_nombres']."</td>";
-echo"<td>".$respuesta['correo']."</td>";
-echo"<td>".$respuesta['telefono']."</td>";
-echo"<td>".$respuesta['direccion']."</td>";
-echo"<td>".$respuesta['id_rol']."</td>";
-echo"<td>".$respuesta['foto']."</td>";
-
-echo"<td><button class='btn btn-primary'>Editar</button><button class='btn btn-danger'>Eliminar</button></td>";
+                        <div class="card">
+                            <div class="card-body">
 
 
 
 
 
-echo"</tr>";
 
 
-    
+                                <?php
 
-}
+                                require('conexion.php');
 
 
-
-?>
-  </tbody>
-</table>
+                                ?>
 
 
 
-<!-- table -->
+
+                                <table id="basic-datatable" class="table dt-responsive nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">id</th>
+                                            <th scope="col">Dni</th>
+                                            <th scope="col">Apellidos_Nombres</th>
+                                            <th scope="col">Correo</th>
+                                            <th scope="col">Telefono</th>
+                                            <th scope="col">Direccion</th>
+                                            <th scope="col">Rol</th>
+                                            <th scope="col">Imagen</th>
+                                            <th scope="col">Accion</th>
+                                        </tr>
+
+
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $consulta = "select *from usuario ";
+                                        $ejecutar = mysqli_query($conn, $consulta);
+                                        $cont = 0;
+
+                                        while ($respuesta = mysqli_fetch_array($ejecutar)) {
+                                            $cont++;
+
+                                            echo "<tr>";
+                                            echo "<td>" . $cont . "</td>";
+                                            echo "<td>" . $respuesta['id'] . "</td>";
+                                            echo "<td>" . $respuesta['dni'] . "</td>";
+                                            echo "<td>" . $respuesta['apellidos_nombres'] . "</td>";
+                                            echo "<td>" . $respuesta['correo'] . "</td>";
+                                            echo "<td>" . $respuesta['telefono'] . "</td>";
+                                            echo "<td>" . $respuesta['direccion'] . "</td>";
+                                            echo "<td>" . $respuesta['id_rol'] . "</td>";
+                                            echo "<td>" . $respuesta['foto'] . "</td>";
+
+                                            echo "<td><button class='btn btn-primary'>Editar</button><button class='btn btn-danger'>Eliminar</button></td>";
+
+
+
+
+
+                                            echo "</tr>";
+                                        }
+
+
+
+                                        ?>
+                                    </tbody>
+                                </table>
+
+
+
+                                <!-- table -->
+                            </div>
+
                         </div>
-                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 </div>
 
+
             </div>
 
-
         </div>
+
 
     </div>
 
 
-</div>
 
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-     <!---FIN DE CONTENIDO-->
+    <!---FIN DE CONTENIDO-->
     <!-- jQuery  -->
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
@@ -312,7 +308,7 @@ echo"</tr>";
 
     <!-- Datatables init -->
     <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
-    
+
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
 
